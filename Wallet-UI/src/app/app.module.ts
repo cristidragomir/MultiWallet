ebaef86100f51e70778bc80e551450cfbfa8f72c
alina-t-872 <alina.tudorache872@gmail.com>
2024-01-16 23:04:38 +0200

    Created components for each page

--- Wallet-UI/src/app/app.module.ts
+++ Wallet-UI/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { CreateAccountPageComponent } from './pages/create-account-page/create-account-page.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    CreateAccountPageComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
